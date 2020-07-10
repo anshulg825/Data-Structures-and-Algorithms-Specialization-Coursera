b44d3089030273d8d8010ed228fee05a22c7bb61
Anshul <anshulg825@gmail.com>
2020-07-10 23:44:09 +0530

    deletion of other language files

--- Coursera Algorithmic ToolBox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ Coursera Algorithmic ToolBox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -2,16 +2,12 @@
 import sys
 
 def get_fibonacci_last_digit_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
+    arr = []
+    arr.append(0)
+    arr.append(1)
+    for i in range(2,n+1):
+    	arr.append( (arr[i - 1]%10) + (arr[i - 2]%10) ) 
+    return (arr[n]%10)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- Coursera Algorithmic ToolBox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ Coursera Algorithmic ToolBox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -2,14 +2,14 @@
 import sys
 
 def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-
-    return a*b
+    for l in range(1, min(a,b)+1):
+        if a % l == 0 and b % l == 0:
+        	gcd = l
+    lcm = (a*b / gcd)
+    return lcm
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(int(lcm_naive(a, b)))
 
