262e1d2834b4ca92ab988a9de386cc21182aed14
Anshul <anshulg825@gmail.com>
2020-07-17 23:50:25 +0530

    week 4

--- Coursera Algorithmic ToolBox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ Coursera Algorithmic ToolBox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -2,13 +2,15 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
-
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
+    left, right = 0, len(a)-1
+    while left<=right:
+        mid = (left + right)//2
+        if a[mid] ==x:
+            return mid
+        if a[mid]>x:
+            right = mid -1
+        else:
+            left = mid + 1
     return -1
 
 if __name__ == '__main__':
@@ -19,4 +21,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
--- Coursera Algorithmic ToolBox/week4_divide_and_conquer/2_majority_element/majority_element.py
+++ Coursera Algorithmic ToolBox/week4_divide_and_conquer/2_majority_element/majority_element.py
@@ -1,18 +1,19 @@
 # Uses python3
 import sys
+from collections import Counter
 
 def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    #write your code here
+    counts = Counter()
+    for x in a:
+        counts[x] += 1
+        if counts[x] > len(a) // 2:
+            return 1
     return -1
-
+    
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n-1) != -1:
         print(1)
     else:
         print(0)
