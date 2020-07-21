4d6c537f24c4a38cff2a0711c1e2de9cb8e48364
Anshul <anshulg825@gmail.com>
2020-07-21 21:12:48 +0530

    week 4

--- Coursera Algorithmic ToolBox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
+++ Coursera Algorithmic ToolBox/week4_divide_and_conquer/4_number_of_inversions/inversions.py
@@ -1,18 +1,41 @@
-# Uses python3
-import sys
+# python3
 
-def get_number_of_inversions(a, b, left, right):
-    number_of_inversions = 0
-    if right - left <= 1:
-        return number_of_inversions
-    ave = (left + right) // 2
-    number_of_inversions += get_number_of_inversions(a, b, left, ave)
-    number_of_inversions += get_number_of_inversions(a, b, ave, right)
-    #write your code here
-    return number_of_inversions
+""" Task: Count the number of inversions of a given sequence """
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *a = list(map(int, input.split()))
-    b = n * [0]
-    print(get_number_of_inversions(a, b, 0, len(a)))
+def merge(left, right):
+    i, j, inversion_counter = 0, 0, 0
+    final = list()
+    while i < len(left) and j< len(right):
+        if left[i] <= right[j]:
+            final.append(left[i])
+            i += 1
+        else:
+            final.append(right[j])
+            inversion_counter += len(left) - i
+            j += 1
+
+    final += left[i:]
+    final += right[j:]
+        
+    return final, inversion_counter
+
+def mergesort(arr):
+    global tot_count
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr)//2
+
+    left = mergesort(arr[:mid])
+    right = mergesort(arr[mid:])
+
+    sorted_arr, temp = merge(left, right)
+    tot_count += temp
+
+    return sorted_arr
+
+tot_count = 0
+n = int(input())
+seq = [int(i) for i in input().split()]
+mergesort(seq)
+print(tot_count)
+
--- Coursera Algorithmic ToolBox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
+++ Coursera Algorithmic ToolBox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.py
@@ -3,15 +3,17 @@ import sys
 
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    #write your code here
+    
     return cnt
 
 def naive_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    for i in range(len(points)):
-        for j in range(len(starts)):
-            if starts[j] <= points[i] <= ends[j]:
-                cnt[i] += 1
+    i,j=0,0
+    while i<len(points) and j<len(starts):
+        if starts[j] <= points[i] <= ends[j]:
+            cnt[i] += 1
+        i+=1
+        j+=1
     return cnt
 
 if __name__ == '__main__':
@@ -26,3 +28,19 @@ if __name__ == '__main__':
     cnt = naive_count_segments(starts, ends, points)
     for x in cnt:
         print(x, end=' ')
+
+
+master_list = list()
+s, p = [int(i) for i in input().split()]
+
+for i in range(s):
+    a, b = [int(i) for i in input().split()]
+    master_list.append((a,'l'))
+    master_list.append((b,'r'))
+
+points = input().split()
+for i in points:
+    master_list.append((int(i),'p'))
+
+master_list.sort()
+print(master_list)
